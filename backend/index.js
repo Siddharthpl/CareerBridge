d189243af89d0c04afaa398cada3d55cef1116fd
Siddharth Pundir <siddharthpundir73@gmail.com>
2025-01-31 19:13:25 +0530

    test3

--- backend/index.js
+++ backend/index.js
@@ -14,17 +14,21 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'https://careerbridge-frontend-eqse.onrender.com',
-    credentials:true
-}
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: [
+      "https://careerbridge-frontend-eqse.onrender.com",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 const PORT = process.env.PORT || 3000;
-
 
 // api's
 app.use("/api/v1/user", userRoute);
@@ -32,9 +36,7 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
+});
